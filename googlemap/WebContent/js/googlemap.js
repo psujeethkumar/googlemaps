3176ec0b659c06dc3a8ed15bb5d6562879b60e2d
KUMAS <KUMAS@ELW01890.rsrc.int>
2014-07-17 10:43:56 +0200

    Rdefactored java script code

--- googlemap/WebContent/js/googlemap.js
+++ googlemap/WebContent/js/googlemap.js
@@ -1,68 +1,129 @@
+/* Current position options */
+var positionOptions = {
+	enableHighAccuracy : true,
+	timeout : 10 * 1000
+};
+
+/* Map options */
+var mapOptions = {
+	zoom : 14,
+};
+
+/* Drawing control options */
+var drawingControlOptions = {
+	position : google.maps.ControlPosition.TOP_CENTER,
+	// drawingModes : [ google.maps.drawing.OverlayType.CIRCLE,
+	// google.maps.drawing.OverlayType.POLYGON,
+	// google.maps.drawing.OverlayType.POLYLINE,
+	// google.maps.drawing.OverlayType.RECTANGLE ]
+	drawingModes : [ google.maps.drawing.OverlayType.CIRCLE ]
+};
+
+/* Circle Options */
+var circleOptions = {
+	draggable : false,
+	editable : true,
+	fillOpacity : 0.1,
+	fillColor : "green",
+	strokeColor : "red"
+};
+
+/* Polygon Options */
+var polygonOptions = {
+	draggable : false,
+	editable : true,
+	fillOpacity : 0.1,
+	fillColor : "green",
+	strokeColor : "red"
+};
+
+/* Polygon Line Options */
+var polygonLineOptions = {
+	draggable : false,
+	editable : true,
+	fillOpacity : 0.1,
+	fillColor : "green",
+	strokeColor : "red"
+};
+
+/* Rectangle Options */
+var rectangleOptions = {
+	draggable : false,
+	editable : true,
+	fillOpacity : 0.1,
+	fillColor : "green",
+	strokeColor : "red"
+};
+
+/* Marker options */
+var markerOptions = {
+	title : "I am here !",
+	animation : google.maps.Animation.BOUNCE
+};
+
+/* Drawing manager options */
+var initalDrawingManagerOptions = {
+	drawingControl : true,
+	drawingControlOptions : drawingControlOptions,
+	circleOptions : circleOptions,
+	polygonOptions : polygonOptions,
+	polygonLineOptions : polygonLineOptions,
+	rectangleOptions : rectangleOptions
+};
+
+/* Drawing manager options after drawing */
+var afterDrawingManagerOptions = {
+	drawingMode : null,
+	drawingControl : false,
+};
+
 /* Function to initialize maps */
 $(document).ready(function() {
-
-	// Check if your device supports geo-locations
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			// Calculate the current location
-			var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-			// Create map options object
-			var mapOptions = {
-				zoom : 14,
-				center : currentLocation
-			};
-
-			// Create drawing manager options
-			var drawingManagerOptions = {
-				drawingMode : google.maps.drawing.OverlayType.CIRCLE,
-				drawingControl : true,
-				drawingControlOptions : {
-					position : google.maps.ControlPosition.TOP_CENTER,
-					drawingModes : [ google.maps.drawing.OverlayType.CIRCLE, google.maps.drawing.OverlayType.RECTANGLE ]
-				},
-				circleOptions : {
-					center : currentLocation,
-					draggable : false,
-					editable : true,
-					fillOpacity : 0.1,
-					fillColor : "green",
-					strokeColor : "red"
-				}
-
-			};
-			var drawingManager = new google.maps.drawing.DrawingManager(drawingManagerOptions);
-
-			// Create map object
-			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-			map.setCenter(currentLocation);
-			var marker = new google.maps.Marker({
-				position : currentLocation,
-				title : "I am here !",
-				animation : google.maps.Animation.BOUNCE,
-			});
-			marker.setMap(map);
-			drawingManager.setMap(map);
-
-			// Listener for listening events when center changed
-			google.maps.event.addListener(map, 'center_changed', function() {
-				window.setTimeout(function() {
-					map.panTo(marker.getPosition());
-				}, 2000);
-			});
-
-			// Listener for listening when circle is drawn
-			google.maps.event.addListener(drawingManager, 'circlecomplete', function(circle) {
-				circle.setCenter(currentLocation);
-				drawingManager.setOptions({
-					drawingControl : false,
-					drawingMode : null
-				});
-			});
-
-		});
+		navigator.geolocation.getCurrentPosition(processIntialMap);
 	} else {
 		error('Your browser do not support this application. Please update !');
 	}
 });
+
+/* Variable to hold current location */
+var currentLocation = null;
+/* Variable to hold map object */
+var map = null;
+/* Variable to hold marker object */
+var marker = null;
+/* Variable to hold drawing manager object */
+var drawingManager = null;
+
+/* Function to process map */
+function processIntialMap(position) {
+	currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	drawingManager = new google.maps.drawing.DrawingManager(initalDrawingManagerOptions);
+	// Create map object
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	// Set center of map to user current location.
+	map.setCenter(currentLocation);
+	marker = new google.maps.Marker(markerOptions);
+	marker.setPosition(currentLocation);
+	// Set marker to map
+	marker.setMap(map);
+	// Add drawing controls to map
+	drawingManager.setMap(map);
+	// Listener for listening events when center changed
+	google.maps.event.addListener(map, 'center_changed', processMapChangedEvent);
+	// Listener for listening when circle is drawn
+	google.maps.event.addListener(drawingManager, 'circlecomplete', processCircleComplete);
+};
+
+/* Method for processing set map center as current location */
+function processMapChangedEvent() {
+	window.setTimeout(function() {
+		map.panTo(marker.getPosition());
+	}, 2000);
+};
+
+/* Method to process event after circle is drawn */
+function processCircleComplete(circle) {
+	circle.setCenter(currentLocation);
+	drawingManager.setOptions(afterDrawingManagerOptions);
+};
